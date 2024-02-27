a81d8249bb75a7814b5252e98a66caab136bc925
z_yu <990860648@qq.com>
2024-02-27 09:25:41 +0800

    2.27

--- app/src/main/java/com/example/platinumhan/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/platinumhan/ui/login/LoginActivity.kt
@@ -1,12 +1,15 @@
 package com.example.platinumhan.ui.login
 
+import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowManager
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -43,57 +46,75 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 import com.example.platinumhan.R
+import com.example.platinumhan.ui.theme.primaryLight
 import com.example.platinumhan.ui.view.Index
+import com.example.platinumhan.ui.view.User
+import java.util.logging.Logger
+import kotlin.math.log
 
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LoginScreen() {
-    val account = remember {
-        mutableStateOf("")
-    }
-    val password = remember {
-        mutableStateOf("")
-    }
+fun LoginScreen(
 
-    var isLogin = remember {
+) {
+
+
+    val isLogin = remember {
         mutableStateOf(false)
     }
+    val user1 = User("ethen", "dsadasfdfsad")
+
+    val context = LocalContext.current
+
+
+    SideEffect {
+        Log.d("LoginScreen", "$context")
+    }
+
 
     if (isLogin.value) {
-        Index()
+        Index(user1)
     } else {
         Surface(
             modifier = Modifier.fillMaxSize(),
-            color = Color.LightGray
+            color = Color.White
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.rectangle_1),
-                contentDescription = "",
-                modifier = Modifier.fillMaxSize(),
-                alignment = Alignment.TopCenter
-            )
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(top = 50.dp),
+                    .padding(top = 50.dp)
+                    .padding(horizontal = 10.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Image(
@@ -102,27 +123,18 @@ fun LoginScreen() {
                     modifier = Modifier
                         .size(100.dp)
                         .clip(RoundedCornerShape(20))
-                        .background(Color(255, 255, 255))
                         .padding(horizontal = 5.dp),
                 )
                 Text(
                     text = "海宁中国家纺城",
                     modifier = Modifier.padding(24.dp),
-                    color = Color.White,
+                    color = Color.LightGray,
                     fontWeight = FontWeight.W500,
                     fontSize = 25.sp
                 )
+                LoginForm(isLogin = isLogin)
+            }
 
-            }
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(horizontal = 10.dp, vertical = 15.dp),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                loginForm(account = account, password = password, isLogin = isLogin)
-            }
         }
     }
 }
@@ -130,81 +142,86 @@ fun LoginScreen() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun loginForm(
-    account: MutableState<String>, password: MutableState<String>,
+fun LoginForm(
     isLogin: MutableState<Boolean>
 ) {
-    Card(
+
+    var count by remember {
+        mutableStateOf(0)
+    }
+
+    val account = remember {
+        mutableStateOf("")
+    }
+    val password = remember {
+        mutableStateOf("")
+    }
+    val localView = LocalView.current;
+
+
+    val context = LocalContext.current
+
+
+    SideEffect {
+        Log.d("LoginForm", "$context")
+    }
+
+
+    TextField(
+        value = account.value,
+        onValueChange = { account.value = it },
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 15.dp)
-                ,
-        colors = CardDefaults.cardColors(containerColor = Color.White)
-    ) {
+            .padding(vertical = 5.dp),
+        placeholder = { Text(text = "手机号") },
+        singleLine = true,
+        colors = TextFieldDefaults.textFieldColors(
+            containerColor = Color.White,
+            placeholderColor = Color.LightGray
+        ),
+    )
+    TextField(
+        value = password.value,
+        onValueChange = {
+            password.value = it
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 5.dp),
+        placeholder = { Text(text = "验证码") },
+        singleLine = true,
+        trailingIcon = {
+            TextButton(
+                onClick = {
 
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                color = Color.Gray,
-                modifier = Modifier.padding(vertical = 16.dp),
-                text = "欢迎回来",
-                fontSize = 15.sp
-            )
-        }
-        OutlinedTextField(value = account.value,
-            onValueChange = { account.value = it },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 15.dp),
-            label = { Text(text = "手机号") },
-            singleLine = true,
-            trailingIcon = {
-                Icon(imageVector = Icons.Filled.Clear, contentDescription = "clear")
-            },
-            supportingText = {
-                Row {
-                    Text(text = "*必要选项")
-                    Spacer(modifier = Modifier.weight(1f))
-                    Text(text = "20/20")
-                }
+                },
+                modifier = Modifier.padding(horizontal = 5.dp)
+            ) {
+                Text(text = "获取验证码")
             }
-        )
-        OutlinedTextField(value = password.value,
-            onValueChange = { password.value = it },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 15.dp),
-            label = { Text(text = "验证码") },
-            singleLine = true,
-            trailingIcon = {
-                TextButton(
-                    onClick = {
-
-                    },
-                    modifier = Modifier.padding(horizontal = 5.dp)
-                ) {
-                    Text(text = "获取验证码")
-                }
-            }
-        )
-        Button(
-            modifier = Modifier
-                .padding(vertical = 20.dp, horizontal = 15.dp)
-                .fillMaxWidth(),
-            elevation = ButtonDefaults.elevatedButtonElevation(),
-            shape = RoundedCornerShape(20),
-            onClick = {
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            containerColor = Color.White,
+            placeholderColor = Color.LightGray
+        ),
+    )
+    Button(
+        modifier = Modifier
+            .padding(vertical = 20.dp, horizontal = 15.dp)
+            .fillMaxWidth(),
+        elevation = ButtonDefaults.elevatedButtonElevation(),
+        shape = RoundedCornerShape(50),
+        onClick = {
 //                isLogin.value = true
-            }) {
-            Text(text = "登录")
-        }
+            count++
+        }) {
+        Text(text = "登录")
     }
 }
 
+
 @Preview
 @Composable
-fun preLogin() {
+fun PreLogin() {
     LoginScreen()
 }
--- app/src/main/java/com/example/platinumhan/ui/view/Index.kt
+++ app/src/main/java/com/example/platinumhan/ui/view/Index.kt
@@ -1,55 +1,102 @@
 package com.example.platinumhan.ui.view
 
 import android.view.Window
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.LifecycleOwner
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+
+@Stable
+data class User(val name: String, val token: String)
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Index() {
+fun Index(user: User,
+
+) {
     val snackbarHostState = remember {
         SnackbarHostState()
     }
-    val isDisplay = remember {
+
+    val reComposableKey = remember {
         mutableStateOf(false)
     }
-    LaunchedEffect(isDisplay.value) {
-        if (isDisplay.value){
 
-            snackbarHostState.showSnackbar("  you see me")
-        }
-    }
+    val stableUser by rememberUpdatedState(newValue = user)
+
+    val scope = rememberCoroutineScope()
+
+//    LaunchedEffect(Unit) {
+//        delay(3000L)
+//        snackbarHostState.showSnackbar(stableUser.name)
+//        println("get it")
+//    }
+
+
+
+
     Scaffold(
         snackbarHost = {
-            SnackbarHost( hostState = snackbarHostState)
-        }
-    ) {
-        innerPadding ->
-        Column(
+            SnackbarHost(hostState = snackbarHostState)
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+    ) { innerPadding ->
+        Surface(
+            onClick = { },
             modifier = Modifier
+                .fillMaxWidth()
                 .padding(innerPadding)
-                .fillMaxSize()
         ) {
+            Row(
 
-            Text(text = "snackBarScreen")
-            Button(onClick = {
-                isDisplay.value = !isDisplay.value
-            }) {
-                Text(text = "显示snackBar")
+            ) {
+
+                Text(text = "${user.name},${user.token}")
+                Button(onClick = {
+                    reComposableKey.value = true
+                    scope.launch {
+                        snackbarHostState.showSnackbar(stableUser.name)
+                    }
+                }) {
+                    Text(text = "显示snackBar")
+                }
             }
         }
 
+
     }
+}
+
+
+@Preview
+@Composable
+fun preIndex() {
+    Index(user = User("ttt", "refdsf"))
 }
